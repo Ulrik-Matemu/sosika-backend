14571b472ac6ffe23d846aad41b8ff40d82e4c3a
Ulrik-Matemu <advancechronicles@gmail.com>
2025-03-12 18:17:53 +0300

    trying to send notifications to users

--- firebase.js
+++ firebase.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const admin = require('firebase-admin');
 
 // Load service account credentials
-const serviceAccount = require('./sosika-101-firebase-adminsdk-fbsvc-59254c2f39.json');  // Ensure this file is in `.gitignore`
+const serviceAccount = require(process.env.FIREBASE_CONFIG);  // Ensure this file is in `.gitignore`
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({

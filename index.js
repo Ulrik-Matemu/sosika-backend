ba99a992a939ba0c55e33aa01d36156f6bdd662e
Ulrik-Matemu <advancechronicles@gmail.com>
2025-03-18 14:22:00 +0300

    implementing wep push #2

--- index.js
+++ index.js
@@ -136,7 +136,12 @@ const deliveryPersonRouter = require('./routes/deliveryPerson');
 const ordersRouter = require("./routes/orders");
 const orderMenuItemsRouter = require("./routes/orderMenuItems");
 
-app.use(cors());
+app.use(cors({
+    // During development, allow requests from your local server
+    origin: ['http://localhost:5173', 'https://ulrik-matemu.github.io'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 app.use(express.json({ limit: "50mb" })); // Increase limit for JSON
 app.use(express.urlencoded({ limit: "50mb", extended: true })); // Increase limit for form data
 app.use(express.json());

8ff1ef288636703b10ca64e7632c6cde3362e023
Ulrik-Matemu <advancechronicles@gmail.com>
2025-03-18 14:04:09 +0300

    implementing wep push

--- routes/orders.js
+++ routes/orders.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db'); // Assuming you have PostgreSQL connection pool setup
 const getIo = require('../socket').getIo;
-const webPush = require('web-push');
 require('dotenv').config();
 const sendVendorNotification = require('../index').sendVendorNotification;
 

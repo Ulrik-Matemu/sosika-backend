57764560bad57aecf5978bc851af3144149b92e6
Ulrik-Matemu <advancechronicles@gmail.com>
2025-04-26 18:04:42 +0300

    adding forgot password feature #3

--- routes/auths.js
+++ routes/auths.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 const pool = require('../db');
 const router = express.Router();
 const { saveToken } = require('../tokenStore');
